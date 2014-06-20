c6ef29fc45ec830f113e6c3d5d42499ee9e3f059
Sebastien Saunier <seb@saunier.me>
2014-06-20 16:00:55 +0200

    Push hook to finish Tracker PR

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -6,13 +6,19 @@ class GithubWebhooksController < ActionController::Base
   def push(payload)
     commit = Commit.new(payload)
     if commit.merge_to_master?
-
+      tracker_project = Tracker::Project.new(@project.user.pivotal_tracker_api_token, @project.tracker_project_id)
+      story = tracker_project.story(commit.story_id)
+      story.finish
+      story.add_label "pull-request"
     end
   end
 
   private
 
   def webhook_secret(payload)
-
+    repo = "#{payload[:repository][:owner][:name]}/#{payload[:repository][:name]}"
+    @project = Project.find_by_repo(repo)
+    raise ActiveRecord::RecordNotFound.new("No project named #{repo} found") unless @project
+    @project.github_webhook_secret
   end
 end
