8b544ad645785bb4567e8e75cf8282602ac2e1ce
Sebastien Saunier <seb@saunier.me>
2014-06-21 19:24:43 +0200

    Spelling mistake

--- app/services/pull_request_closer.rb
+++ app/services/pull_request_closer.rb
@@ -5,7 +5,7 @@ class PullRequestCloser < PullRequestActionBase
     client = Octokit::Client.new access_token: @project.user.github_token
     if client.pull_merged?(@project.repo, pull_request.number)
       story.finish
-      story.add_note "#{pull_request_markdown} has been **merged**, marking story has **Finished**."
+      story.add_note "#{pull_request_markdown} has been **merged**, marking story as **finished**."
     else
       story.add_note "#{pull_request_markdown} has been closed (and **not** merged)."
     end
