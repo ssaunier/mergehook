9cc150ad33f5ffff41cd9a73d64c5c461686339a
Sebastien Saunier <seb@saunier.me>
2014-06-22 19:12:05 +0200

    HTTPS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.default_url_options = { host: ENV['HOST'], protocol: ENV['PROTOCOL'] }
 end
