84a5c2ba9bde2b0081e6010babb4f069f628b0a6
Sebastien Saunier <seb@saunier.me>
2014-06-22 19:26:37 +0200

    Force ssl if protocol set to HTTPS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV['HOST'], protocol: ENV['PROTOCOL'] }
+  config.force_ssl = ENV['PROTOCOL'] == 'https'
 end
