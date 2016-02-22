22f4a9b78a529f2af6b7d9831c3833c7b24e1ce1
Robert Grigore <robertgrig@gmail.com>
2016-02-22 16:26:25 +0200

    update pull request description

--- app/services/pull_request_creator.rb
+++ app/services/pull_request_creator.rb
@@ -5,6 +5,12 @@ class PullRequestCreator < PullRequestActionBase
     if pull_request
       story.add_label LABEL
       story.add_note "#{pull_request_markdown} has been opened."
+      update_pull_request_description
     end
   end
+
+  def update_pull_request_description
+    client.update_pull_request @project.repo, pull_request.number, nil, pull_request.story_id
+  end
+
 end
